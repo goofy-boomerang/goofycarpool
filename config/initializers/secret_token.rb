7d93040180189bcb07434e4d40fd8eaf309e2cff
Goofy <sion@goofyboomerang.com>
2014-07-01 14:44:11 +0530

    Token
    Token changed

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -5,4 +5,4 @@
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
 #Icare::Application.config.secret_token = APP_CONFIG.secret_token
-Goofycarpool::Application.config.secret_token = "jbvjasfomnbdvdgfirf78ty7483hrh4tyihy4ytrfghiuty4yhergry"
+Icare::Application.config.secret_token = "e5d6fed054aadcb955c17d90ab0cae35b9921e3af7fa0f88ba2b8bcee6ab1b087e34c2081c867b42093f81197bd4272c49a2337fc7d31b6f892ae6ef57e842d7"
